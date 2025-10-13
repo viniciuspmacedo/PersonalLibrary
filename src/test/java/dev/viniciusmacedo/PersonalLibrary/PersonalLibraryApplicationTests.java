414f1990b323efac5302bae660a989d74648cec0
Vinicius Macedo <viniciusp.macedo@gmail.com>
2025-10-13 08:21:14 -0300

    Initial commit

---
+++ src/test/java/dev/viniciusmacedo/PersonalLibrary/PersonalLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.viniciusmacedo.PersonalLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
